9bd32c58e2445e5a264269d7896793003f9d21c2
Nishant garg <iamnishantgarg@gmail.com>
2019-11-24 16:50:36 +0530

    prompt for username

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const socket = io("http://localhost:3000");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 const messageContainer = document.getElementById("message-container");
+const name = prompt("what is your name..?");
+appendMessage("you joined");
+socket.emit("new-user", name);
 socket.on("chat-message", data => {
   //   console.log(data);
   appendMessage(data);
 });
+
+socket.on("user-connected", name => {
+  appendMessage(`${name} connected`);
+});
+
 messageForm.addEventListener("submit", e => {
   e.preventDefault();
   const message = messageInput.value;
